b4a252cf2811db60b5759c01e0ff6d9dcd6477cf
madprops <manuelchaves@gmail.com>
2018-10-28 14:13:19 -0600

    More stuff.

--- doctor.js
+++ doctor.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-const doctor_version = "v1.0.0-rc.23"
+const doctor_version = "v1.0.0-rc.24"
 const doctor_site_url = "https://madprops.github.io/Doctor/"
 
 const time_start = Date.now()
